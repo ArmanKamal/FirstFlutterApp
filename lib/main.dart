f5883903d13fe39bfbe0053bda6bdebe9b6c5be6
ArmanKamal <armankamal2421@gmail.com>
2021-11-22 23:54:09 -0600

    scaffold app

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,19 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Center(
-      child: Text("Hello world"),
+      home: Scaffold(
+    appBar: AppBar(
+      title: Text('Rich App'),
+      backgroundColor: Colors.purple.shade800,
     ),
-  ));
+    body: Center(
+      child: Image(
+          image: NetworkImage(
+              'https://images.unsplash.com/photo-1637520942665-9a4f2d0b08f8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80'
+          ),
+  )
+    )
+      )
+  )
+  );
 }
